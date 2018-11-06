558761bb93f3a3760345fb080fd5236408a1ee8e
eothein <jens.buysse@gmail.com>
2018-11-06 11:40:15 +0100

    Extracted the extensions functions from the MainActivity.

--- app/src/main/java/be/equality/metar/activities/MainActivity.kt
+++ app/src/main/java/be/equality/metar/activities/MainActivity.kt
@@ -1,20 +1,15 @@
 package be.equality.metar.activities
 
+
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentTransaction
-import android.support.v7.app.AppCompatActivity;
-
-
-
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
-import be.equality.metar.*
-
+import android.support.v7.app.AppCompatActivity
+import be.equality.metar.R
 import be.equality.metar.fragments.*
-import com.orhanobut.logger.Logger
 import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.main_layout.*
 
 
@@ -26,38 +21,6 @@ class MainActivity : AppCompatActivity(), AirportsFragment.OnFragmentInteraction
 OldmetarsFragment.OnFragmentInteractionListener,RawFragment.OnFragmentInteractionListener{
 
 
-
-    /**
-     * Extension function to the FragmentManager which accepts a Lambda with Receiver as its argument, whereas the FragmentTransaction is the Receiver.
-     * Here the parameter func is an Extension function to the FragmentTransaction
-     * and it has zero parameters and returns a [FragmentTransaction]. We invoke that function after calling
-     * the beginTransaction() and end the transaction by calling commit().
-     *
-     * Sauce: [https://medium.com/thoughts-overflow/how-to-add-a-fragment-in-kotlin-way-73203c5a450b]
-     */
-    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
-        val fragmentTransaction = beginTransaction()
-        fragmentTransaction.func()
-        fragmentTransaction.commit()
-    }
-
-    /**
-     * Extension functions to the AppCompatActivity which adds a fragment
-     *
-     * Sauce: [https://medium.com/thoughts-overflow/how-to-add-a-fragment-in-kotlin-way-73203c5a450b]
-     */
-    fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int) {
-        supportFragmentManager.inTransaction { add(frameId, fragment) }
-    }
-
-    /**
-     * Extension functions to the AppCompatActivity which replaces a fragment
-     *
-     * Sauce: [https://medium.com/thoughts-overflow/how-to-add-a-fragment-in-kotlin-way-73203c5a450b]
-     */
-    fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int) {
-        supportFragmentManager.inTransaction { replace(frameId, fragment) }
-    }
 
 
     /**
